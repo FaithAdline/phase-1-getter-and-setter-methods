2568dd0c96404548965bfb4fc71ca60ae31fcb62
FaithAdline <adlinefaith@gmail.com>
2023-10-18 11:13:13 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+  
+    get diameter() {
+      return this.radius * 2;
+    }
+  
+    set diameter(diameter) {
+      this.radius = diameter / 2;
+    }
+  
+    get circumference() {
+      return Math.PI * this.diameter;
+    }
+  
+    set circumference(circumference) {
+      this.radius = circumference / Math.PI / 2;
+    }
+  
+    get area() {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+  
+    set area(area) {
+      this.radius = Math.sqrt(area / Math.PI);
+    }
+  }
+  
+  // Example usage
+  const myCircle = new Circle(5);
+  console.log('Radius:', myCircle.radius);
+  console.log('Diameter:', myCircle.diameter);
+  console.log('Circumference:', myCircle.circumference);
+  console.log('Area:', myCircle.area);
+  
+  // Set new values for diameter, circumference, and area
+  myCircle.diameter = 10;
+  console.log('New Radius (after setting diameter):', myCircle.radius);
+  
+  myCircle.circumference = 31.4159;
+  console.log('New Radius (after setting circumference):', myCircle.radius);
+  
+  myCircle.area = 78.5398;
+  console.log('New Radius (after setting area):', myCircle.radius);
+  
